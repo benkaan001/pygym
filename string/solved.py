42193ebc43ccf8e34ea13431f8a76aa4a1db41aa
benkaan001 <benkaan001@gmail.com>
2022-08-17 00:01:04 -0500

    add string 16-17

--- string/solved.py
+++ string/solved.py
@@ -1,12 +1,13 @@
 # (1) Write a program to create a new string made of an input string’s first, middle, and last character.
-str1 = "festive" # fte
+import re
+str1 = "festive"  # fte
 
 new_str = str1[0] + str1[len(str1) // 2] + str1[len(str1)-1]
 print(new_str)
 
 # (2) Write a program to create a new string made of the middle three characters of an input string.
-str1 = "JaSonAy" # Son
-str1 = "JohonDipPeta" # Dip
+str1 = "JaSonAy"  # Son
+str1 = "JohonDipPeta"  # Dip
 
 mi = len(str1) // 2
 output = str1[mi-1:mi+2]
@@ -14,7 +15,7 @@ output = str1[mi-1:mi+2]
 
 # (3) Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
 s1 = "Aunt"
-s2 = "Kelly" # AuKellynt
+s2 = "Kelly"  # AuKellynt
 
 middle = len(s1) // 2
 new_str = s1[:middle] + s2 + s1[middle:]
@@ -23,23 +24,24 @@ print(new_str)
 
 # (4) Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
 s1 = "America"
-s2 = "Japan" # AJrpan
+s2 = "Japan"  # AJrpan
 
-transformed = s1[0] + s2[0] + s1[len(s1) // 2] + s2[len(s2) // 2] + s1[len(s1)-1] + s2[len(s2) -1]
+transformed = s1[0] + s2[0] + s1[len(s1) // 2] + \
+    s2[len(s2) // 2] + s1[len(s1)-1] + s2[len(s2) - 1]
 print(transformed)
 
 
 # (5) Write a program to arrange the characters of a string so that all lowercase letters should come first.
-str1 = "PyNaTive" # yaivePNT
+str1 = "PyNaTive"  # yaivePNT
 
 lower = []
 upper = []
 
 for i in str1:
-   if i.islower():
-    lower.append(i)
-   else:
-     upper.append(i)
+    if i.islower():
+        lower.append(i)
+    else:
+        upper.append(i)
 
 joined = "".join(lower+upper)
 print(joined)
@@ -72,7 +74,7 @@ print(f"Characters={len(char)}\nDigits={len(digit)}\nSymbols={len(symbol)}")
 s1 = "Abc"
 s2 = "Xyz"  # AzbycX
 
-joined = "".join( (s1[i] + s2[len(s2)-1-i]) for i in range(len(s1)))
+joined = "".join((s1[i] + s2[len(s2)-1-i]) for i in range(len(s1)))
 print(joined)
 
 # (8) Write a program to check if two strings are balanced.
@@ -81,15 +83,16 @@ print(joined)
 s1 = "Yn"
 s2 = "PYnative"  # True
 s1 = "Ynf"
-s2 = "PYnative" # False
+s2 = "PYnative"  # False
 
-if s1 in s2: print(True)
-else: print(False)
-
+if s1 in s2:
+    print(True)
+else:
+    print(False)
 
 
 # (9) Write a program to find all occurrences of “USA” in a given string ignoring the case.
-str1 = "Welcome to USA. usa awesome, isn't it?" # count = 2
+str1 = "Welcome to USA. usa awesome, isn't it?"  # count = 2
 
 count = str1.upper().count("USA")
 print(count)
@@ -99,7 +102,7 @@ print(count)
 # that appear in the string, ignoring all other characters.
 str1 = "PYnative29@#8496"  # Sum is: 38 Average is  6.333333333333333
 
-digits = [ int(i) for i in str1 if i.isdigit() ] # [2, 9, 8, 4, 9, 6]
+digits = [int(i) for i in str1 if i.isdigit()]  # [2, 9, 8, 4, 9, 6]
 
 digits_sum = sum(digits)
 digits_avg = digits_sum / len(digits)
@@ -109,21 +112,20 @@ print(f"Sum is: {digits_sum}\nAverage is: {digits_avg}")
 # (11) Write a program to count occurrences of all characters within a string
 str1 = "Apple"  # {'A': 1, 'p': 2, 'l': 1, 'e': 1}
 
-str_dic = { letter: str1.count(letter) for letter in str1 }
+str_dic = {letter: str1.count(letter) for letter in str1}
 print(str_dic)
 
 
 # (12) Reverse a given string
 str1 = "PYnative"   # evitanYP
 
-str1_reversed = "".join(sorted(str1, reverse = True))
+str1_reversed = "".join(sorted(str1, reverse=True))
 str1_reversed = str1[::-1]
 print(str1_reversed)
 
 # most cost effective
 str1 = "".join(reversed(str1))
 print(str1)
-
 
 
 # (13) Write a program to find the last position of a substring “Emma” in a given string.
@@ -134,11 +136,9 @@ index = str1.rfind("Emma")
 print(f"Last occurrence of Emma starts at index {index}")
 
 
-
 # (14) Split a string on hyphens
-str1 = "Emma-is-a-data-scientist" # Emmaisadatascientist
-print( "".join(str1.split("-")) )
-
+str1 = "Emma-is-a-data-scientist"  # Emmaisadatascientist
+print("".join(str1.split("-")))
 
 
 # (15)  Remove empty strings from a list of strings
@@ -153,3 +153,15 @@ booled = list(filter(lambda x: bool(x) == True, str_list))
 print(booled)
 
 
+# (16) Remove special symbols / punctuation from a string
+str1 = "/*Jon is @develo^^^per & musician&&"     # "Jon is developer musician"
+
+clean_str1 = re.sub(r'[^\w\s]', '', str1)
+print(clean_str1)
+
+
+# (17) Removal all characters from a string except integers
+str1 = "She is 25 years and 10 months old"  # 2510
+
+filtered = "".join(list(filter(lambda x: x.isdigit(), str1)))
+print(filtered)
--- string/unsolved.py
+++ string/unsolved.py
@@ -1,35 +1,29 @@
 # (1) Write a program to create a new string made of an input string’s first, middle, and last character.
-str1 = "festive" # fte
-
+str1 = "festive"  # fte
 
 
 # (2) Write a program to create a new string made of the middle three characters of an input string.
-str1 = "JaSonAy" # Son
-str1 = "JohonDipPeta" # Dip
-
+str1 = "JaSonAy"  # Son
+str1 = "JohonDipPeta"  # Dip
 
 
 # (3) Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
 s1 = "Aunt"
-s2 = "Kelly" # AuKellynt
-
+s2 = "Kelly"  # AuKellynt
 
 
 # (4) Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
 s1 = "America"
-s2 = "Japan" # AJrpan
-
+s2 = "Japan"  # AJrpan
 
 
 # (5) Write a program to arrange the characters of a string so that all lowercase letters should come first.
-str1 = "PyNaTive" # yaivePNT
-
+str1 = "PyNaTive"  # yaivePNT
 
 
 # (6) Count all letters, digits, and special symbols from a given string
 # Characters=8 Digits=3 Symbols=4
 str1 = "P@#yn26at^&i5ve"
-
 
 
 # (7) Given two strings, s1 and s2. Write a program to create a new string s3
@@ -40,7 +34,6 @@ s1 = "Abc"
 s2 = "Xyz"  # AzbycX
 
 
-
 # (8) Write a program to check if two strings are balanced.
 # For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2.
 # The character’s position doesn’t matter.
@@ -48,13 +41,11 @@ s1 = "Yn"
 s2 = "PYnative"  # True
 
 s1 = "Ynf"
-s2 = "PYnative" # False
-
+s2 = "PYnative"  # False
 
 
 # (9) Write a program to find all occurrences of “USA” in a given string ignoring the case.
-str1 = "Welcome to USA. usa awesome, isn't it?" # count = 2
-
+str1 = "Welcome to USA. usa awesome, isn't it?"  # count = 2
 
 
 # (10) Given a string s1, write a program to return the sum and average of the digits
@@ -62,16 +53,12 @@ str1 = "Welcome to USA. usa awesome, isn't it?" # count = 2
 str1 = "PYnative29@#8496"  # Sum is: 38 Average is  6.333333333333333
 
 
-
 # (11) Write a program to count occurrences of all characters within a string
 str1 = "Apple"  # {'A': 1, 'p': 2, 'l': 1, 'e': 1}
 
 
-
 # (12) Reverse a given string
 str1 = "PYnative"   # evitanYP
-
-
 
 
 # (13) Write a program to find the last position of a substring “Emma” in a given string.
@@ -79,13 +66,18 @@ str1 = "Emma is a data scientist who knows Python. Emma works at google."
 # Last occurrence of Emma starts at index 43
 
 
-
 # (14) Split a string on hyphens
-str1 = "Emma-is-a-data-scientist" # Emmaisadatascientist
-
+str1 = "Emma-is-a-data-scientist"  # Emmaisadatascientist
 
 
 # (15) Remove empty strings from a list of strings
 str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
 # ['Emma', 'Jon', 'Kelly', 'Eric']
 
+
+# (16) Remove special symbols / punctuation from a string
+str1 = "/*Jon is @develo^^^per & musician&&"     # "Jon is developer musician"
+
+
+# (17) Removal all characters from a string except integers
+str1 = "She is 25 years and 10 months old"  # 2510
