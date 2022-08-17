41515313cfe12a9ddbfd868ca86e5fb00ddd2207
benkaan001 <benkaan001@gmail.com>
2022-08-16 22:03:09 -0500

    add string 4-5

--- string/solved.py
+++ string/solved.py
@@ -2,7 +2,6 @@
 str1 = "festive" # fte
 
 new_str = str1[0] + str1[len(str1) // 2] + str1[len(str1)-1]
-
 print(new_str)
 
 # (2) Write a program to create a new string made of the middle three characters of an input string.
@@ -15,12 +14,33 @@ output = str1[mi-1:mi+2]
 
 # (3) Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
 s1 = "Aunt"
-s2 = "Kelly" #AuKellynt
+s2 = "Kelly" # AuKellynt
 
 middle = len(s1) // 2
 new_str = s1[:middle] + s2 + s1[middle:]
+print(new_str)
+
 
 # (4) Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
 s1 = "America"
-s2 = "Japan" #AJrpan
+s2 = "Japan" # AJrpan
 
+transformed = s1[0] + s2[0] + s1[len(s1) // 2] + s2[len(s2) // 2] + s1[len(s1)-1] + s2[len(s2) -1]
+print(transformed)
+
+
+# (5) Write a program to arrange the characters of a string so that all lowercase letters should come first.
+
+str1 = "PyNaTive" # yaivePNT
+
+lower=[]
+upper=[]
+
+for i in str1:
+   if i.islower():
+    lower.append(i)
+   else:
+     upper.append(i)
+
+joined = "".join(lower+upper)
+print(joined)
--- string/unsolved.py
+++ string/unsolved.py
@@ -12,10 +12,15 @@ str1 = "JohonDipPeta" # Dip
 
 # (3) Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
 s1 = "Aunt"
-s2 = "Kelly" #AuKellynt
+s2 = "Kelly" # AuKellynt
 
 
 
 # (4) Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
 s1 = "America"
-s2 = "Japan" #AJrpan
+s2 = "Japan" # AJrpan
+
+
+# (5) Write a program to arrange the characters of a string so that all lowercase letters should come first.
+str1 = "PyNaTive" # yaivePNT
+
