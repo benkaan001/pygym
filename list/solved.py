e719536e97448234bc2181431b9aff9f29bd61cb
benkaan001 <benkaan001@gmail.com>
2022-08-18 22:48:03 -0500

    add list 5-7

--- list/solved.py
+++ list/solved.py
@@ -40,3 +40,37 @@ list2 = ["Dear", "Sir"]
 
 nested_list = [i + y for i in list1 for y in list2]
 print(nested_list)
+
+
+# (5) Given a two Python list. Write a program to iterate both lists simultaneously
+# and display items from list1 in original order and items from list2 in reverse order.
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]  # 10 400   20 300   30 200   40 100
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+
+# (6) Remove empty strings from the list of strings
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# ['Mike', 'Emma', 'Kelly', 'Brad']
+
+filtered_list = list(filter(None, list1))
+print(filtered_list)
+
+# more Pythonic
+filtered_list = [i for i in list1 if bool(i) == True]
+print(filtered_list)
+
+
+# (7) Write a program to add item 7000 after 6000 in the following Python List
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+
+list1[2][2].append(7000)
+print(list1)
+
+# using insert
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list1[2][2].insert(2, 7000)
+print(list1)
--- list/unsolved.py
+++ list/unsolved.py
@@ -17,3 +17,19 @@ numbers = [1, 2, 3, 4, 5, 6, 7]  # [1, 4, 9, 16, 25, 36, 49]
 list1 = ["Hello ", "take "]
 list2 = ["Dear", "Sir"]
 # ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+
+
+# (5) Given a two Python list. Write a program to iterate both lists simultaneously
+# and display items from list1 in original order and items from list2 in reverse order.
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]  # 10 400   20 300   30 200   40 100
+
+
+# (6) Remove empty strings from the list of strings
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# ['Mike', 'Emma', 'Kelly', 'Brad']
+
+
+# (7) Write a program to add item 7000 after 6000 in the following Python List
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
