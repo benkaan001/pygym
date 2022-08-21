2d823c69ce48f1603bdebfcefa1c8c152e81d371
benkaan001 <benkaan001@gmail.com>
2022-08-20 23:28:09 -0500

    add set 3-4

--- set/solved.py
+++ set/solved.py
@@ -28,3 +28,36 @@ print(union)
 
 # using built-in intersection method
 print(set1.intersection(set2))
+
+
+# (3) Return a new set with unique items from both sets by removing duplicates.
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+# {70, 40, 10, 50, 20, 60, 30}
+
+combined = set1.union(set2)
+print(combined)
+
+
+combined2 = set([*set1, *set2])
+print(combined2)
+
+
+# (4) Update the first set with items that don’t exist in the second set
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+# set1 {10, 30}
+
+# method 1
+set1 = set1 - set2
+print(set1)  # {10, 30}
+
+# using difference_update method
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+difference = set1.difference(set2)
+print(difference)  # {10, 30}
+print(set1)  # {10, 20, 30}
+
+set1.difference_update(set2)
+print(set1)  # {10, 30}
--- set/unsolved.py
+++ set/unsolved.py
@@ -8,3 +8,15 @@ sample_list = ["Blue", "Green", "Red"]
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 # {40, 50, 30}
+
+
+# (3) Return a new set with unique items from both sets by removing duplicates.
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+# {70, 40, 10, 50, 20, 60, 30}
+
+
+# (4) Update the first set with items that don’t exist in the second set
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+# set1 {10, 30}
