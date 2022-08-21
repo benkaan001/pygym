ba02cf3a0d08514ef5398a203bdab0407245f0a1
benkaan001 <benkaan001@gmail.com>
2022-08-20 22:47:10 -0500

    add tupple 5-10

--- tuple/solved.py
+++ tuple/solved.py
@@ -25,5 +25,48 @@ tuple1 = (30,)
 tuple1 = (10, 20, 30, 40)  # a = 10, b = 20, c = 30 , d = 40
 
 a, b, c, d = tuple1
-
 print(f"a={a}, b={b}, c={c}, d={d}")
+
+
+# (5) Swap given two tuples
+tuple1 = (11, 22)       # tuple1 = (99,88)
+tuple2 = (99, 88)       # tuple2 = (11,22)
+
+tuple1, tuple2 = tuple2, tuple1
+print(f"tuple1={tuple1}, tuple2={tuple2}")
+
+
+# (6) Write a program to copy elements 44 and 55 from the following tuple into a new tuple.
+tuple1 = (11, 22, 33, 44, 55, 66)  # tuple2: (44, 55)
+
+tuple2 = tuple1[3:5]
+tuple2 = tuple1[3:-1]
+print(tuple2)
+
+
+# (7) Write a program to modify the first item (22) of a list inside a following tuple to 222
+tuple1 = (11, [22, 33], 44, 55)
+# tuple1: (11, [222, 33], 44, 55)
+
+tuple1[1][0] = 222
+print(tuple1)
+
+
+# (8) Sort a tuple of tuples by 2nd item
+tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29))
+# (('c', 11), ('a', 23), ('d', 29), ('b', 37))
+
+sorted_tuple = tuple(sorted(tuple1, key=lambda x: x[1]))
+print(sorted_tuple)
+
+
+# (9) Counts the number of occurrences of item 50 from a tuple
+tuple1 = (50, 10, 60, 70, 50)
+
+print(tuple1.count(50))
+
+
+# (10) Check if all items in the tuple are the same
+tuple1 = (45, 45, 45, 45)
+
+print(True) if len(tuple1) != len(set(tuple1)) else print(False)
--- tuple/unsolved.py
+++ tuple/unsolved.py
@@ -12,3 +12,30 @@ tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
 
 # (4) Write a program to unpack the following tuple into four variables and display each variable.
 tuple1 = (10, 20, 30, 40)  # a = 10, b = 20, c = 30 , d = 40
+
+
+# (5) Swap given two tuples
+tuple1 = (11, 22)       # tuple1 = (99,88)
+tuple2 = (99, 88)       # tuple2 = (11,22)
+
+
+# (6) Write a program to copy elements 44 and 55 from the following tuple into a new tuple.
+tuple1 = (11, 22, 33, 44, 55, 66)  # tuple2: (44, 55)
+
+
+# (7) Write a program to modify the first item (22) of a list inside a following tuple to 222
+tuple1 = (11, [22, 33], 44, 55)
+# tuple1: (11, [222, 33], 44, 55)
+
+
+# (8) Sort a tuple of tuples by 2nd item
+tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29))
+# (('c', 11), ('a', 23), ('d', 29), ('b', 37))
+
+
+# (9) Counts the number of occurrences of item 50 from a tuple
+tuple1 = (50, 10, 60, 70, 50)
+
+
+# (10) Check if all items in the tuple are the same
+tuple1 = (45, 45, 45, 45)
