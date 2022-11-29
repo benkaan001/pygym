8770117d264db86629878daea5e75cd2e5d7e17a
benkaan001 <benkaan001@gmail.com>
2022-11-28 22:10:07 -0600

    add states data

---
+++ states/00_test.py
@@ -0,0 +1,10 @@
+from states_data import state_list,abbreviations, populations, capitals, admissions
+
+print(capitals['az']) # Phoenix
+
+# from admissions print out the full name of the youngest state
+
+# print the population difference between the most and least populated states
+
+
+# print the capital of the oldest state along with its population and year of admission
