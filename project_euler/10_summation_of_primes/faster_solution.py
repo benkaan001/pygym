79a0cbbd33db346e269fb8415e240ae6b27e8096
benkaan001 <benkaan001@gmail.com>
2022-12-06 00:25:41 -0600

    add faster solution for summation of primes

---
+++ project_euler/10_summation_of_primes/faster_solution.py
@@ -0,0 +1,23 @@
+import math
+
+RANGE_VALUE = 2000000
+
+def is_prime(num):
+    
+    #check for factors
+    max_factors = math.floor(math.sqrt(num))
+    for i in range(2, 1 + max_factors):
+        if num % i == 0:
+            return False 
+    return True 
+
+# create a generator object that will lazily evaluate the prime numbers
+primes = (i for i in range(RANGE_VALUE +1) if is_prime(i))
+print(primes) # <generator object <genexpr> at 0x108c05270>
+
+# print the sum
+try:
+    total = sum(primes)
+    print(total) # 142913828922
+except Exception as e:
+    print(e)
