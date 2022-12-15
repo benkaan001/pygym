3882fd9fff1e2d3c52cd5c7de1368dce2f4a3acd
benkaan001 <benkaan001@gmail.com>
2022-12-15 15:06:27 -0600

    update solution for 29

--- project_euler/29_distinct_powers/solution.py
+++ project_euler/29_distinct_powers/solution.py
@@ -1,4 +1,8 @@
+# time module to calculate the total time the solution takes
+import time 
 
+# start time
+start = time.time()
 
 nums = []
 
@@ -8,4 +12,11 @@ for i in range(2,101):
         if num not in nums:
             nums.append(num)
 
+print(nums)
 print(len(nums)) #9183 
+
+# end time
+end = time.time()
+
+print(f"Solution took {end - start} second(s)")
+# Solution took 0.7711269855499268 second(s)
